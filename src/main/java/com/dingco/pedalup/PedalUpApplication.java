6f962e33e665ee11cb0b16e16561d3764198b6e4
briso05 <sobright05@naver.com>
2022-06-29 00:23:26 +0900

    create project

---
+++ src/main/java/com/dingco/pedalup/PedalUpApplication.java
@@ -0,0 +1,13 @@
+package com.dingco.pedalup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedalUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedalUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingco/pedalup/PedalUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingco.pedalup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
